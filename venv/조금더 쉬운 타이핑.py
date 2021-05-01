91c5b79f82866f63b7b61f2067dffffb95c66522
rheeh <yooziwoo.coding0987@gmail.com>
2021-05-01 11:10:14 +0900

    으엌 힘들어

---
+++ venv/조금더 쉬운 타이핑.py
@@ -0,0 +1,34 @@
+"""이번에도 미쳤다...(지난번보단 나은데...)"""
+# ㅋㅋㅋㅋ
+
+
+empty_list = list()
+print(len(empty_list))
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']
+web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB']
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+
+print('과일:', fruits)
+print('과일 갯수:', len(fruits))
+print('야채:', vegetables)
+print('야채 갯수:', len(vegetables))
+print('동물 식품:', animal_products)
+print('동물 식품 갯수:', len(animal_products))
+print('인터넷 기술:', web_techs)
+print('인터넷 기술 갯수:', len(web_techs))
+print('국가:', countries)
+print('국가 갯수:', len(countries))
+
+
+fruits_fruit = fruits[0]
+print(fruits_fruit)
+fruits_fruit = fruits[1]
+print(fruits_fruit)
+fruits_fruit = fruits[3]
+print(fruits_fruit)
+
+last_index = len(fruits) - 1
+last_fruit = fruits[last_index]
